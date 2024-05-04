810dfc11209a787537d8c32d5c11422df3a94cea
euricodande12 <euricodande12@gmail.com>
2024-05-04 20:15:11 +0200

    Taking photos and storing(PT-1)

---
+++ fisrt-app/src/hooks/usePhotoGallery.ts
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+import { isPlatform } from '@ionic/react';
+
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Preferences } from '@capacitor/preferences';
+import { Capacitor } from '@capacitor/core';
+
+export function usePhotoGallery() {
+    const [photos, setPhotos] = useState<UserPhoto[]>([]);
+
+    const takePhoto = async () => {
+        const photo = await Camera.getPhoto({
+            resultType: CameraResultType.Uri,
+            source: CameraSource.Camera,
+            quality: 100,
+        });
+
+        const fileName = Date.now() + '.jpeg';
+        const newPhotos = [
+        {
+            filepath: fileName,
+            webviewPath: photo.webPath,
+        },
+        ...photos,
+        ];
+        setPhotos(newPhotos);
+        
+        if (photo && photo.path) {
+            const newPhoto: UserPhoto = {
+                filepath: photo.path,
+                webviewPath: Capacitor.convertFileSrc(photo.path),
+            };
+
+            setPhotos(prevPhotos => [...prevPhotos, newPhoto]);
+        } else{
+            console.error("Failed to capture photo or obtain photo path.");
+        }
+    };
+
+    return {
+        photos,
+        takePhoto,
+    };
+};
+
+
+export interface UserPhoto {
+    filepath: string;
+    webviewPath?: string;
+}
