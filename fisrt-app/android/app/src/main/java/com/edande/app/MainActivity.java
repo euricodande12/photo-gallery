455e7703e538e6326695b20af6d98077796703a9
euricodande12 <euricodande12@gmail.com>
2024-12-18 02:53:41 +0100

    Restarting...

--- fisrt-app/android/app/src/main/java/com/edande/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.edande.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- fisrt-app/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.edande.app',
-  appName: 'fisrt-app',
-  webDir: 'build'
-};
-
-export default config;
